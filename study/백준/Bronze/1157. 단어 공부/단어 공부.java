e28e0d6200fa49fbb310b7263e05db3e0da0c69b
jyoonje <nobuts98@gmail.com>
2023-12-07 10:32:30 +0900

    aa

---
+++ study/백준/Bronze/1157. 단어 공부/단어 공부.java
@@ -0,0 +1,54 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception{
+		
+		   BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	        String input = br.readLine();
+		
+	        input = input.toLowerCase();	 // 대소문자를 구분하지 않기 위해 소문자로 통일
+	        int[] frequencyArray = new int[26];	// 알파벳 빈도를 저장하는 배열 (a~z: 26개)
+
+	        for (char c : input.toCharArray()) {	// 각 알파벳의 빈도 계산
+	            if (Character.isLetter(c)) {
+	                int index = c - 'a';	// a의 ASCII값: 97 b의 ASCII값 : 98, c: 99 ... z: 122	
+	                frequencyArray[index]++;
+	            }
+	        }
+		
+	        int maxFrequency = 0;	// 빈도가 가장 높은 값 찾기
+	        for (int frequency : frequencyArray) {
+	            maxFrequency = Math.max(maxFrequency, frequency);	// 두 값 중 더 큰 값으로 maxFrequency 업데이트	
+	        }
+
+		//String mostFrequentCharacters = "";	// 가장 높은 빈도를 가지는 알파벳들을 저장하는 문자열 생성
+	        StringBuilder mostFrequentCharacters = new StringBuilder();
+
+	        for (char c = 'a'; c <= 'z'; c++) {
+	            int index = c - 'a';
+	            if (frequencyArray[index] == maxFrequency) {
+			//mostFrequentCharacters += c;    
+	                mostFrequentCharacters.append(c);
+	            }
+	        }
+
+	        if (mostFrequentCharacters.length() == 1) {	// 결과 출력. 빈도가 가장높은 문자의 집합이기 때문에 하나를 초과하면 '?' 출력
+	            System.out.println(Character.toUpperCase(mostFrequentCharacters.charAt(0)));
+	        } else {
+	            System.out.println("?");
+	        }
+		
+	}
+
+}
+
+
+
+
+
+
+
