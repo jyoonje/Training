0bd62066fafc7ac84c526bfa2b085350bd42e5ec
jyoonje <150825231+jyoonje@users.noreply.github.com>
2023-12-20 10:56:32 +0900

    [Silver V] Title: 소트인사이드, Time: 132 ms, Memory: 14268 KB -BaekjoonHub

---
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.java
@@ -0,0 +1,42 @@
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception{
+		//선택정렬 알고리즘 연습
+		// 1<= N <=1,000,000,000
+		// N을 입력받고, 배열로 변환 -> 내림차순으로 정렬 
+		//													ㄴ 최댓값을 기준으로 정렬
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String N =br.readLine();
+		int[] numArr = new int[N.length()];
+		
+		for(int i=0; i<numArr.length; i++) {
+			numArr[i] = N.charAt(i) - '0';
+		}
+		
+		for(int i=0; i<numArr.length; i++) {
+			int maxIdx = i;
+			for(int j=i+1; j<numArr.length; j++){
+				if(numArr[j] > numArr[maxIdx]) {
+					maxIdx = j;
+				}
+			}
+			if(numArr[i]<numArr[maxIdx]) {
+				int tmp=numArr[i]; numArr[i]=numArr[maxIdx]; numArr[maxIdx]=tmp;
+			}
+		}
+		for(int item : numArr) {
+			System.out.print(item);
+		}
+	}
+
+}
+
+
+
+
+
