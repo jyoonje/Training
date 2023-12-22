0a391f09a56fe5f08cea85c38cecc3f9faaf3c0f
jyoonje <150825231+jyoonje@users.noreply.github.com>
2023-12-22 15:23:57 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 124 ms, Memory: 14276 KB -BaekjoonHub

---
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String str = br.readLine();
+		
+		char[] A = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+
+		StringBuilder sb = new StringBuilder();
+		for(int i=0; i<A.length; i++) {
+			sb.append(str.indexOf(A[i])).append(" ");
+		}
+		System.out.println(sb);
+	}
+
+}
