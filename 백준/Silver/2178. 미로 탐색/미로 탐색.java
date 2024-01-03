1271e2b3dc0a53d5bed0d801aab9ae13380a5ca3
jyoonje <150825231+jyoonje@users.noreply.github.com>
2024-01-03 21:01:02 +0900

    [Silver I] Title: 미로 탐색, Time: 156 ms, Memory: 15424 KB -BaekjoonHub

---
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -0,0 +1,73 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+// 시간제한 1초	
+// N, M(2 ≤ N, M ≤ 100)
+// BFS 알고리즘 연습
+	
+	static int[] dx = {0, 1, 0, -1};
+	static int[] dy = {1, 0, -1, 0};	// 좌표를 상하좌우로 이동하기 위한 배열 dx, dy
+	static boolean[][] visited;
+	static int[][] A;
+	static int N, M;
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		
+		A = new int[N][M];
+		visited = new boolean[N][M];
+		
+		for(int i=0; i<N; i++) {		//배열 초기화
+			st = new StringTokenizer(br.readLine());
+			String str = st.nextToken();
+			for(int j=0; j<M; j++) {
+				A[i][j] = Integer.parseInt(str.substring(j, j+1));
+			}
+		}
+		
+		BFS(0, 0);
+		System.out.println(A[N-1][M-1]);
+	}
+	
+	static void BFS(int i, int j) {
+		Queue<int[]> q = new LinkedList<>();
+		q.offer(new int[] {i,j});
+		visited[i][j] = true;
+		
+		while(!q.isEmpty()) {
+			int now[] = q.poll();
+			
+			for(int k=0; k<4; k++) {
+				int x = now[0] + dx[k];
+				int y = now[1] + dy[k];
+				
+				if(x>=0 && y>=0 && x<N && y<M) {
+					if(A[x][y] != 0 && !visited[x][y]) {
+						visited[x][y] = true;
+						A[x][y] = A[now[0]][now[1]] + 1;
+						q.add(new int[] {x, y});
+					}
+				}
+				
+			}
+		}
+	}
+
+}
+
+
+
+
+
+
+
+
+
