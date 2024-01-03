1c9a39c5301c4762d22b84976af214e812fed106
jyoonje <150825231+jyoonje@users.noreply.github.com>
2024-01-03 20:59:02 +0900

    [Gold V] Title: 신기한 소수, Time: 792 ms, Memory: 14116 KB -BaekjoonHub

---
+++ 백준/Gold/2023. 신기한 소수/신기한 소수.java
@@ -0,0 +1,55 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+	//일의자리 수 중 2, 3, 5, 7이 소수이므로, 시간복잡도를 줄이기 위해 불필요한 1, 4, 6, 8, 9는 함수 호출하지 않음
+	
+	static int N;
+
+	public static void main(String[] args) throws Exception{
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+		
+		DFS(2, 1);
+		DFS(3, 1);
+		DFS(5, 1);
+		DFS(7, 1);
+	}
+	
+	static void DFS(int number, int jarisu) {
+		if(jarisu == N) {
+			if(isPrime(number)) {
+				System.out.println(number);
+			}
+			return;
+		}
+		
+		for(int i=1; i<10; i++) {
+			if(i % 2 == 0) {	//짝수라면 탐색x
+				continue;	
+			}
+			if(isPrime(number*10 + i)) {
+				DFS(number*10 + i, jarisu+1);
+			}
+		}
+	}
+	
+	static boolean isPrime(int num) {	//  소수 구하는 메소드
+		for(int i=2; i<=num/2; i++) { 	//	num/2 이상부터는 확인할 필요 없음
+			if(num % i == 0) {
+				return false;	
+			}
+		}
+		return true;
+	}
+
+}
+
+
+
+
+
+
+
+
