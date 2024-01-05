aa0724227fb29cf1c1e414720439beb963019f3a
jyoonje <150825231+jyoonje@users.noreply.github.com>
2024-01-05 17:48:34 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 124 ms, Memory: 14292 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
@@ -2,9 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.StringTokenizer;
-import java.util.stream.Collectors;
 
 public class Main {
 
@@ -17,26 +15,25 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		str = br.readLine();
 
-		st = new StringTokenizer(str, "-");
+		st = new StringTokenizer(str, "-");			// 입력 문자열을 "-" 를 기준으로 자른다.
 		list = new ArrayList<>();
-		while (st.hasMoreTokens()) {
+		while (st.hasMoreTokens()) {					// 자른 문자열을 list에 넣는다.
 			list.add(st.nextToken());
 		}
 
 		int sum = 0;
-
-		for (int i = 1; i < list.size(); i++) {
+		for (int i = 1; i < list.size(); i++) {			// list의 요소들의 덧셈 계산을 수행하는 calculator() 메소드 실행
 			sum += calculator(list.get(i));
 		}
 
-		if (!str.startsWith("-")) {
-			System.out.println(calculator(list.get(0)) - sum);
+		if (!str.startsWith("-")) {													// 입력 문자열이 "+"로 시작하면 첫번째 인덱스 - 나머지
+			System.out.println(calculator(list.get(0)) - sum);	// 입력문자열이 "-"로 시작하면 0-모든 인덱스
 		} else {
 			System.out.println(0 - calculator(list.get(0)) - sum);
 		}
 	}
 
-	static int calculator(String element) {
+	static int calculator(String element) {						//리스트의 요소를 "+" 기준으로 나눠서 요소의 덧셈을 수행한 후 반환
 		ArrayList<Integer> list = new ArrayList<>();
 		int sum = 0;
 		
