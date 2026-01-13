bc900d64597d88408bf03849130ca5d59abd32ba
jyoonje <150825231+jyoonje@users.noreply.github.com>
2026-01-13 21:04:20 +0900

    [Bronze II] Title: 막대기, Time: 200 ms, Memory: 22604 KB -BaekjoonHub

---
+++ 백준/Bronze/17608. 막대기/막대기.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int N = Integer.parseInt(br. readLine());
+
+    int[] arr = new int[N];
+    for(int i = 0; i < N; i++){
+      arr[i] = Integer.parseInt(br.readLine());
+    }
+
+    int rightNum = arr[N-1];
+    int cnt = 1;
+
+    for(int i = N-2; i >= 0; i--){
+      if (arr[i] > rightNum){
+        cnt++;
+        rightNum = arr[i];
+      }
+    }
+    System.out.println(cnt);
+
+  }
+}
