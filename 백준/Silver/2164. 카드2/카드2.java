5cb49c0052bfd5202480c1ce43e965ec5eee7d63
jyoonje <150825231+jyoonje@users.noreply.github.com>
2026-01-13 21:24:06 +0900

    [Silver IV] Title: 카드2, Time: 164 ms, Memory: 28960 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.java
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -1,39 +1,23 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
+import java.util.ArrayDeque;
 import java.util.Queue;
 
 public class Main {
 
-	public static void main(String[] args) throws Exception{
-		// TODO FIFO의 문제이다.
-		//		두 번 버리고, 나중에 버린 카드를 다시 삽입한다.
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int N = Integer.parseInt(br.readLine());
+    Queue<Integer> queue = new ArrayDeque<>();
+    for(int i = 1; i <= N; i++){
+      queue.add(i);
+    }
 
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		int N = Integer.parseInt(br.readLine());
-		br.close();
-		
-		//1~N까지 큐에 삽입
-		Queue<Integer> cardQueue = new LinkedList<>();
-		for(int i=1; i<=N; i++) {
-			cardQueue.offer(i);
-		}
-		
-		//큐의 길이가 1일 때 까지 반복
-		while(cardQueue.size() > 1) {
-			cardQueue.poll();
-			cardQueue.offer(cardQueue.poll());
-		}
-		
-		//마지막에 남은 값 출력
-		System.out.println(cardQueue.peek());
-	}
+    while(queue.size() > 1){
+      queue.poll();
+      queue.add(queue.poll());
+    }
+    System.out.println(queue.poll());
+  }
 }
-
-
-
-
-
-
-
