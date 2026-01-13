9ff610a3cc53ceefe8af22614715d0507d05360c
jyoonje <150825231+jyoonje@users.noreply.github.com>
2026-01-13 22:36:43 +0900

    [Silver V] Title: 소트인사이드, Time: 104 ms, Memory: 14200 KB -BaekjoonHub

--- 백준/Silver/1427. 소트인사이드/소트인사이드.java
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.java
@@ -1,36 +1,30 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
 
-	public static void main(String[] args) throws Exception {
-		// 선택정렬 알고리즘 연습
-		// 1<= N <=1,000,000,000
-		// N을 입력받고, 배열로 변환 -> 내림차순으로 정렬
-		// ㄴ 최댓값을 기준으로 정렬
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    String line = br.readLine();
+    int[] arr = new int[line.length()];
 
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String N = br.readLine();
-		int[] numArr = new int[N.length()];
+    for (int i = 0; i < line.length(); i++) {
+      arr[i] = line.charAt(i) - '0';
+    }
 
-		for (int i = 0; i < numArr.length; i++) {
-			numArr[i] = N.charAt(i) - '0';
-		}
-
-		for (int i = 0; i < numArr.length; i++) {
-			int maxIdx = i;
-			for (int j = i + 1; j < numArr.length; j++) {    //최댓값 찾기
-				if (numArr[j] > numArr[maxIdx]) {
-					maxIdx = j;
-				}
-			}
-			int tmp = numArr[i];        //swap
-			numArr[i] = numArr[maxIdx];
-			numArr[maxIdx] = tmp;
-		}
-		for (int item : numArr) {
-			System.out.print(item);
-		}
-	}
-
+    for(int i = 0; i < arr.length-1; i++){
+      int tmp;
+      for(int j = i+1; j < arr.length; j++){
+        if(arr[j] > arr[i]){
+          tmp = arr[i];
+          arr[i] = arr[j];
+          arr[j] = tmp;
+        }
+      }
+    }
+    for (int n : arr) {
+      System.out.print(n);
+    }
+  }
 }
